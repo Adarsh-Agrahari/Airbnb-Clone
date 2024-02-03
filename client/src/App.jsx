fb472a24d1adcf1ae1bec40ddff260207e602b84
Adarsh Agrahari <adarshagrahari0503@gmail.com>
2024-02-03 17:43:20 +0530

    Added router for Header and LoginPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import {Route, Router, Routes} from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <Routes>
       <Route index element={<IndexPage/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
     </Routes>
     
   );
