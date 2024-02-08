acec91d954ab708d74021be6b841df5cefed7db1
Adarsh Agrahari <adarshagrahari0503@gmail.com>
2024-02-09 02:28:08 +0530

    Added route for subpages in Account Page

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,14 +1,41 @@
 import { useContext } from "react";
 import { UserContext } from "../userContext";
-import { Navigate } from "react-router-dom";
+import { Link, Navigate, useParams } from "react-router-dom";
 
 export default function AccountPage() {
   const { ready, user } = useContext(UserContext);
+
+  let { subpage } = useParams();
+  if (subpage === undefined) subpage = "profile";
+
   if (!ready) {
     return "Loading...";
   }
+
   if (ready && !user) {
     return <Navigate to={"/login"} />;
   }
-  return <div>Account Page for {user.name}</div>;
+
+  function linkClasses(type = null) {
+    let classes = "py-2 px-6";
+    if (type === subpage)
+      classes += "py-2 px-6 text-white bg-primary rounded-full";
+    return classes;
+  }
+
+  return (
+    <div>
+      <nav className="w-full flex justify-center mt-4 gap-2 ">
+        <Link className={linkClasses("profile")} to={"/account"}>
+          My Profile
+        </Link>
+        <Link className={linkClasses("bookings")} to={"/account/bookings"}>
+          My Bookings
+        </Link>
+        <Link className={linkClasses("places")} to={"/account/places"}>
+          My Accomodations
+        </Link>
+      </nav>
+    </div>
+  );
 }
