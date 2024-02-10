a6a6615d8c6fe059ae58ba64257565f2045bcf4c
Adarsh Agrahari <adarshagrahari0503@gmail.com>
2024-02-10 16:58:23 +0530

    Added title, address link & photos in PlacePage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,8 +27,8 @@ function App() {
           <Route path="/account/places" element={<PlacesPage />} />
           <Route path="/account/bookings" element={<BookingsPage />} />
           <Route path="/account/places/new" element={<PlacesFormPage />} />
+          <Route path="/account/places/:id" element={<PlacesFormPage />} />
           <Route path="/place/:id" element={<PlacePage />} />
-
         </Route>
       </Routes>
     </UserContextProvider>
